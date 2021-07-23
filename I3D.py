dc4892b5f8c69475143e30b5c3d492b5a8e3217c
Reem Alfaifi <87594794+ReemAlfaifi@users.noreply.github.com>
2021-07-23 17:13:36 +0300

    Update I3D.py

--- I3D.py
+++ I3D.py
@@ -32,12 +32,8 @@ def pre_processing(x_train, x_val):
    x_val=x_val-mean_t
    x_val=x_val/std_t
    return x_train, x_val
-def model ():    
-   x_true, x_pred, y_true, y_pred = div_data.div_train_val (total_samples, split_idx, num_frm, img_size, num_ch)
-   x_true, x_pred= pre_processing(x_true, x_pred)
-    
-   x_true= np.reshape(x_true, (batch_train , num_frm, img_size, img_size, num_ch))
-   x_pred= np.reshape(x_pred, (batch_val , num_frm, img_size, img_size, num_ch))
+
+def proposed_model ():    
 
    input_1 = tf.keras.layers.Input((num_frm, img_size, img_size, num_ch))
    model = hub.KerasLayer('https://tfhub.dev/deepmind/i3d-kinetics-400/1', trainable=False)
@@ -48,10 +44,15 @@ def model ():
    output=tf.keras.layers.Dense(nb_classes)(dropout)
    model = tf.keras.Model(inputs=input_1, outputs=output)
      
-   return model
+   return model, x_true,  y_true, x_pred, y_pred
+
 for split_idx in range (0, 5):        
-    prposed_model=model()
-    prposed_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+    model=proposed_model()
+    
+    x_true, x_pred, y_true, y_pred = div_data.div_train_val (total_samples, split_idx, num_frm, img_size, num_ch)
+    x_true, x_pred= pre_processing(x_true, x_pred)
+    
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
     checkpoint = tf.keras.callbacks.ModelCheckpoint(get_model_name(split_idx), monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
-    prposed_model.summary()
-    history = prposed_model.fit(x_true,  y_true, epochs=500, validation_data=(x_pred, y_pred), batch_size=16, callbacks=[checkpoint], verbose=2)
+    model.summary()
+    history = model.fit(x_true,  y_true, epochs=500, validation_data=(x_pred, y_pred), batch_size=16, callbacks=[checkpoint], verbose=2)
